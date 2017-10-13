b5844eebc851403e64de3f6a751f504f7332fd6c
Braulio Brunaud <brunaud@cmu.edu>
2017-10-13 15:35:57 -0400

    removed D example

--- src/benders.jl
+++ src/benders.jl
@@ -18,32 +18,6 @@ function isChildNode(g::PlasmoGraph, n1::PlasmoNode, n2::PlasmoNode)
   return false
 end
 
-##Place MP and SP into PlasmoGraph
-mp = Model(solver = GurobiSolver())
-sp = Model(solver = GurobiSolver())
-
-@variable(mp,y>=0)
-@objective(mp,Min,2y)
-
-@variable(sp,x[1:2]>=0)
-@variable(sp,y>=0)
-@constraint(sp,c1,2x[1]-x[2]+3y>=4)
-@constraint(sp,x[1]+2x[2]+y>=3)
-@objective(sp,Min,2x[1]+3x[2])
-
-## Plasmo Graph
-g = PlasmoGraph()
-g.solver = GurobiSolver()
-n1 = add_node(g)
-setmodel(n1,mp)
-n2 = add_node(g)
-setmodel(n2,sp)
-
-##Set n2 as a child node of n1
-edge = Plasmo.add_edge(g,n1,n2)
-
-## Linking constraints between MP and SP
-@linkconstraint(g, n1[:y] == n2[:y])
 
 function benderssolve(graph::PlasmoGraph;max_iterations=2)
 
@@ -57,14 +31,12 @@ function benderssolve(graph::PlasmoGraph;max_iterations=2)
   @variable(mp,θ>=0)
   mp.obj += θ
 
-  solve(mp)
-  
-  for iter in 1:max_iterations 
+  for iter in 1:max_iterations
     solve(mp)
     #Start iterating through the linking constraints
     links = getlinkconstraints(g)
     len = length(links)
-    
+
     for j in 1:len
       #Iterate through each variable in the linked constraint
       for i in 1:length(links[j].terms.vars)
@@ -77,13 +49,14 @@ function benderssolve(graph::PlasmoGraph;max_iterations=2)
         if isChildNode(g,spnode,varnode)
           #copy variable value and add dual constraint to sp
           val = getvalue(var)
+          ## QUESTION: Could also use @variable(sp, valbar == val)
           @variable(sp,val<=valbar<=val)
           if (getnode(links[j].terms.vars[i+1])==spnode)
               @constraint(sp,dual,valbar-links[j].terms.vars[i+1] == 0)
           elseif (getnode(links[j].terms.vars[i-1])==spnode)
               @constraint(sp,dual,valbar-links[j].terms.vars[i+1] == 0)
           end
-          
+
           status = solve(sp)
           λ = getdual(dual)
 
@@ -106,5 +79,3 @@ function benderssolve(graph::PlasmoGraph;max_iterations=2)
     end
   end
 end
-
-benderssolve(g)
