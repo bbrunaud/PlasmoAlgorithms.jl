197b551471bb327adaa549c16b7bd878c32a1d14
Braulio Brunaud <brunaud@cmu.edu>
2018-04-19 17:19:52 -0400

    New benders ongoing

--- src/utils.jl
+++ src/utils.jl
@@ -13,3 +13,20 @@ function normalizegraph(graph::PlasmoGraph)
     graph.attributes[:normalized] = n
     return n
 end
+
+getx(graph::PlasmoGraph) = graph.attributes[:x]
+getλ(graph::PlasmoGraph) = graph.attributes[:λ]
+getrootnode(graph::PlasmoGraph) = graph.attributes[:rootnode]
+getnodebounds(graph::PlasmoGraph) = graph.attributes[:nodebounds]
+getnodeobjectivevals(graph::PlasmoGraph) = graph.attributes[:nodeobjectivevals]
+getlevels(graph::PlasmoGraph) = graph.attributes[:levels]
+
+function turnoffpresolve(model::JuMP.Model)
+    if typeof(model.solver) == CPLEX.CplexSolver
+        push!(model.solver.options,(:CPX_PARAM_PREIND,0))
+    elseif typeof(model.solver) == Gurobi.GurobiSolver
+        push!(model.solver.options,(:Presolve,0))
+    else
+        error("Root relaxation requires to turn off presolve for the solver, only CPLEX and Gurobi are supported")
+    end
+end
