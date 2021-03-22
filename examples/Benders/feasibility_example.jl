21fb809beb4a4b795f29c8d2adef3aa23399f0dc
Brunaud, Braulio [JJCUS] <BBrunau1@ITS.JNJ.com>
2021-03-21 22:42:32 -0400

    Gurobi environment issue handled

--- examples/Benders/feasibility_example.jl
+++ examples/Benders/feasibility_example.jl
@@ -5,17 +5,18 @@ using PlasmoAlgorithms
 
 graph = OptiGraph()
 optimizer = Gurobi.Optimizer
-Plasmo.set_optimizer(graph, optimizer)
+const env = Gurobi.Env()
+Plasmo.set_optimizer(graph, with_optimizer(optimizer, env))
 
 mp = @optinode(graph)
-JuMP.set_optimizer(mp.model, optimizer)
-set_optimizer_attribute(mp, "InfUndbRay", 1)
+JuMP.set_optimizer(mp.model, with_optimizer(optimizer, env))
+set_optimizer_attribute(mp.model, "InfUnbdInfo", 1)
 @variable(mp, y <= 5)
 @objective(mp, Max, y)
 
 sp = @optinode(graph)
-JuMP.set_optimizer(sp.model, optimizer)
-set_optimizer_attribute(sp, "InfUndbRay", 1)
+JuMP.set_optimizer(sp.model, with_optimizer(optimizer, env))
+set_optimizer_attribute(sp.model, "InfUnbdInfo", 1)
 @variables(sp, begin
     v >= 1
     y >= 0
@@ -25,3 +26,11 @@ end)
     v <= y + 3
 end)
 @objective(sp, Max, v)
+
+edge1 = add_edge!(graph, [mp,sp])
+
+@linkconstraint(graph, mp[:y] == sp[:y])
+
+r = bendersoptimize!(graph, max_iterations = 5)
+
+Gurobi.free_env(env)
